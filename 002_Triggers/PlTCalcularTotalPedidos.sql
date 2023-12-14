e78125c79187f12449b6a731fa071d5ae0e1be6f
mcirillojr <142939090+mcirillojr@users.noreply.github.com>
2023-12-14 10:58:08 -0300

    Correção Trigger
    Correção Trigger

--- 002_Triggers/PlTCalcularTotalPedidos.sql
+++ 002_Triggers/PlTCalcularTotalPedidos.sql
@@ -1,15 +1,25 @@
-CREATE OR REPLACE TRIGGER calcular_total_pedido_trigger
+CREATE OR REPLACE TRIGGER HR.calcular_total_pedido_trigger
 AFTER INSERT ON itens_pedido
-FOR EACH ROW
 DECLARE
-    v_pedido_id pedidos.pedido_id%TYPE;
-BEGIN
-    -- Obtendo o pedido_id do novo item inserido
-    v_pedido_id := :new.pedido_id;
+    CURSOR c_itens_pedido IS
+        SELECT DISTINCT pedido_id FROM itens_pedido;
 
-    -- Atualizando o total do pedido
-    UPDATE pedidos
-    SET total_pedido = calcular_total_pedido(v_pedido_id)
-    WHERE pedido_id = v_pedido_id;
+    v_pedido_id pedidos.pedido_id%TYPE;
+    v_total_pedido NUMBER(15,3);
+BEGIN
+    
+    FOR r_pedido IN c_itens_pedido LOOP
+        v_pedido_id := r_pedido.pedido_id;
+
+        -- Chamando a função calcular_total_pedido diretamente usando variáveis locais
+        SELECT calcular_total_pedido(v_pedido_id) INTO v_total_pedido FROM dual;
+
+        -- Atualizando o total do pedido
+        UPDATE pedidos
+        SET total_pedido = v_total_pedido
+        WHERE pedido_id = v_pedido_id;
+    END LOOP;
 END;
 /
+
+
